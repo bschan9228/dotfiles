0177874d63ddd3bc18d444afb345ff03a22a6c1d
bschan9228 <bschan9228@gmail.com>
2026-04-19 01:11:11 -0400

    updated vim

--- modules/editor.nix
+++ modules/editor.nix
@@ -5,6 +5,16 @@
     enable = true;
     extraConfig = ''
       set rnu
+      set expandtab
+      set cursorcolumn
+      set noswapfile
+      set hlsearch " `Ctrl-l to clear highlighted search
+      " set incsearch " this one is not as good
+      set autoindent
+      set backspace=indent,eol,start
+      set ruler
+      set splitright
+      set splitbelow
     '';
   };
 }
