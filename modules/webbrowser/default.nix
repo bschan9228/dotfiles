31b4ccc22a96ca043f1bb5708bdb086c38d832f5
bschan9228 <bschan9228@gmail.com>
2026-05-20 21:36:48 -0400

    added bookmarks to another file

--- modules/webbrowser/default.nix
+++ modules/webbrowser/default.nix
@@ -1,6 +1,10 @@
 { config, pkgs, inputs, ... }:
 
  {
+  imports = [
+    ./bookmarks.nix
+  ];
+
   programs.firefox = {
     enable = true;
     profiles.default = {
@@ -40,74 +44,6 @@
         # keepa #TODO: unfree
       ];
 
-      bookmarks = {
-        force = true;
-        settings = [
-          {
-            name = "Misc";
-            bookmarks = [
-              {
-                name = "wikipedia";
-                tags = [ "wiki" ];
-                keyword = "wiki";
-                url = "https://www.wikipedia.com";
-              }
-              {
-                name = "google";
-                url = "https://www.google.com";
-              }
-              {
-                name = "translate";
-                url = "https://www.translate.google.com";
-              }
-              {
-                name = "maps";
-                url = "https://www.maps.google.com";
-              }
-              {
-                name = "youtube";
-                url = "https://www.Youtube.com";
-              }
-              {
-                name = "gitlab";
-                url = "https://www.gitlab.com";
-              }
-              {
-                name = "github";
-                url = "https://www.github.com";
-              }
-              {
-                name = "amazon";
-                url = "https://www.amazon.com";
-              }
-              {
-                name = "onshape";
-                url = "https://www.cad.onshape.com";
-              }
-              {
-                name = "reddit";
-                url = "https://www.reddit.com";
-              }
-              {
-                name = "yahoo finance";
-                url = "https://www.finance.yahoo.com";
-              }
-              {
-                name = "aliexpress";
-                url = "https://www.aliexpress.com";
-              }
-              {
-                name = "chatgpt";
-                url = "https://www.chatgpt.com";
-              }
-              {
-                name = "ebay";
-                url = "https://www.ebay.com";
-              }
-            ];
-          }
-        ];
-      };
     };
   };
 }
