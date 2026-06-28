17ac7fd360a447073e6a6958eda20211128f07e1
bschan9228 <bschan9228@gmail.com>
2026-06-27 23:52:08 -0400

    updated git settings

--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -11,6 +11,11 @@
       };
       init.defaultBranch = "main";
       core.editor = "vim";
+      remote.origin.fetch = "+refs/heads/*:refs/remotes/origin/*"; # git worktree fetch
+
+      config.pull.ff = "only";
+      # config.pull.rebase = true; # might be better to do ff only
+
     };
 
     ignores = [
