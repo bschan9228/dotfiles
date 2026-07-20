0a93d8e07f2aa1265e44a24db8b7d585f4b366e8
bschan9228 <bschan9228@gmail.com>
2026-07-20 16:04:00 -0400

    added kicad settings

--- modules/home-manager/cad/kicad.nix
+++ modules/home-manager/cad/kicad.nix
@@ -2,4 +2,8 @@
 
 {
   #TODO: design rule templates?
+  home.file.".config/kicad/10.0/eeschema.json".source =
+    config.lib.file.mkOutOfStoreSymlink
+      #TODO: this path needs to be relative
+      "${config.home.homeDirectory}/.dotfiles/modules/home-manager/cad/kicad/eeschema.json";
 }
--- modules/home-manager/git/default.nix
+++ modules/home-manager/git/default.nix
@@ -24,6 +24,7 @@
       ".*"
       "!*.example"
       "!.gitignore"
+      "!.envrc"
 
       # emacs
       "\#*\#"
